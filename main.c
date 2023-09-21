aacc75c0dab197f4564f8d583f84b946be3ee569
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-09-21 16:07:46 +0900

    2

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	int input_int;
-	float input_float;
+int main(int argc, char *argv[])
+{
+	char c;
+	int i;
 	
-	printf("enter an integer : ");
-	scanf("%d", &input_int);
+	printf("input a number : ");
+	scanf("%c", &c);
 	
+	i = c - '0';
+	printf("The input number ASCII is %i\n", c);
+	printf("The input number is %i\n", i);
 	
-	printf("enter a float : ");
-	scanf("%f", &input_float);
-	
-	printf(":integer : %d, float : %f\n", input_int, input_float);
-	
+	system("PAUSE");
 	return 0;
 }
