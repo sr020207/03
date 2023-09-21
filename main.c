2c101af80bfe38154af2caeee15beac36a869c08
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-09-21 16:44:13 +0900

    4 나누기

--- main.c
+++ main.c
@@ -3,13 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-   char c;
+   float x, y; 
    
-   printf("input a character : "); 
-   scanf("%c", &c); 
-    
- 
-   printf("the nect character of %c (%d) is (%d)\n", c, c, c+1, c+1);
+   printf("분자를 입력하세요 : "); 
+   scanf("%f", &x); 
+   
+   printf("분모를 입력하세요 : "); 
+   scanf("%f", &y); 
+   
+   //printf("%f\n", x); //확인용 코드 
+   //printf("%f\n", y); //확인용 코드 
+   
+   printf("나누기의 결과는 %f 입니다.\n", x/y); 
+   
    
    system("PAUSE");
    return 0;
